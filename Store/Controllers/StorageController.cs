dfd0815e40560762d2e8b9a8d00eb161f776b33c
Artem <mygoditsfake2@gmail.com>
2019-05-18 20:46:21 +0500

    Добавьте файлы проекта.

---
+++ Store/Controllers/StorageController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store.Models;
+
+namespace Store.Controllers
+{
+    public class StorageController : Controller
+    {
+        Context db = new Context();
+
+        [HttpGet]
+        public ActionResult StoragePage()
+        {
+            return View(db.Products);
+        }
+
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.Status = true; // при добавлении модели статус становится "в наличии"
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("StoragePage");
+            }
+            else
+                return View();
+        }
+
+        [HttpGet]
+        public ActionResult RemoveProduct()
+        {
+            return View(db.Products);
+        }
+        
+        [HttpPost]
+        public ActionResult RemoveProduct(int ID)
+        {
+            Product deletedProduct = db.Products.Find(ID);
+            if(deletedProduct != null)
+            {
+                db.Products.Remove(deletedProduct);
+                db.SaveChanges();
+            }
+            return RedirectToAction("StoragePage");
+        }
+    }
+}
---
+++ Store/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="Укажите название")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Укажите описание")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage ="Укажите цену")]
+        [Range(0, int.MaxValue, ErrorMessage ="Неверно указанная сумма")]
+        public int Price { get; set; }
+
+        public bool Status { get; set; }
+
+    }
+}
