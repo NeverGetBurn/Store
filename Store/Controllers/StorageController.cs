2dfddef4b35358d10d7c130c48dab28573dc2679
Artem <mygoditsfake2@gmail.com>
2019-05-19 23:33:10 +0500

    Добавил поиск товаров по названию и описанию

--- Store/Controllers/StorageController.cs
+++ Store/Controllers/StorageController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Store.Models;
+using System.Data.Entity;
 
 namespace Store.Controllers
 {
@@ -15,6 +17,34 @@ namespace Store.Controllers
         public ActionResult StoragePage()
         {
             return View(db.Products);
+        }
+
+        [HttpPost]
+        public ActionResult SearchProduct(string searchString, string action)
+        {
+            if (action == "byName")
+            {
+                var prod = from p in db.Products
+                           select p;
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    prod = prod.Where(s => s.Name.Contains(searchString));
+                }
+                return View("StoragePage", prod);
+            }
+
+            else if (action == "byDescription")
+            {
+                var prod = from p in db.Products
+                           select p;
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    prod = prod.Where(s => s.Description.Contains(searchString));
+                }
+                return View("StoragePage", prod);
+            }
+
+            else return View("StoragePage"); // не решил как завершить эту ветку кода
         }
 
         [HttpGet]
