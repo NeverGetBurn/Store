2db563deaf0e12e16c73ffaf2aa279d9b4ffed6f
bercut <bercut@A35FB>
2019-11-11 01:25:21 +0500

    Syncronizer console project

---
+++ Store.DAL.Syncronizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Store.DAL.Syncronizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
