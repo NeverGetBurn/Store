a5854aaffe0e825262855bbc05d358190ea5b58c
bercut <bercut@A35FB>
2019-11-11 21:30:14 +0500

    memory storage

--- Store.CORE/Interfaces/IStorage.cs
+++ Store.CORE/Interfaces/IStorage.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Linq;
 namespace Store.CORE.Interfaces
 {
+    using System.Collections.Generic;
     using Models;
     public interface IStorage
     {
          IQueryable<Product> Products{get;}
+         Product Insert(Product value);
+         ICollection<Product> Insert(IEnumerable<Product> value);
+         bool Update(Product value);
+         int Update(IEnumerable<Product> values);
+         
     }
 }
---
+++ Store.DAL.Memory/Store.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Store.CORE.Interfaces;
+using Store.CORE.Models;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Concurrent;
+
+namespace Store.DAL.Memory
+{
+    public class Store:IStorage
+    {
+        private readonly Dictionary<int,Product> _products = new Dictionary<int, Product>();
+        private readonly object _lock = new object();
+        private int _maxId = 0;
+        public Product Insert(Product value)
+        {
+            lock(_lock){
+                value.Id = _maxId;
+                _maxId++;
+                _products.Add(value.Id,value);
+            }
+            return value;
+        }
+
+        public ICollection<Product> Insert(IEnumerable<Product> value)
+        {
+            var result = new List<Product>();
+            return value.Select(s=>Insert(s)).ToArray();
+        }
+
+        public bool Update(Product value)
+        {
+            lock(_lock){
+                if(!_products.ContainsKey(value.Id)){
+                    return false; 
+                }
+                _products[value.Id]=value;
+            }
+            return true;
+        }
+
+        public int Update(IEnumerable<Product> values) => values.Select(s=>Update(s)).Where(w=>w == true).Count();
+        public IQueryable<Product> Products => _products.Values.AsQueryable();
+
+    }
+}
